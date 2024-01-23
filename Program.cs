0e914dfc11ed4f503576d3310e15757d8193fb94
mbeksz <mustaafabatun@gmail.com>
2024-01-23 14:53:00 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OgrenciSistemi
+{
+    class Program
+    {
+        static Dictionary<string, string> kullaniciBilgileri = new Dictionary<string, string>();
+        static Dictionary<string, Dictionary<int, ogrenci>> ogrenciBilgileri = new Dictionary<string, Dictionary<int, ogrenci>>();
+        static string aktifOgretmen;
+
+        static void Main()
+        {
+            KullaniciBilgileriEkle("Ece", "12366");
+            KullaniciBilgileriEkle("Musti", "78900");
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Kullanıcı Adı: ");
+                string kullaniciAdi = Console.ReadLine();
+
+                Console.Write("Şifre: ");
+                string sifre = Console.ReadLine();
+
+                if (KullaniciGirisDogrulama(kullaniciAdi, sifre))
+                {
+                    Console.WriteLine("Giriş başarılı. Uygulamaya hoş geldiniz.");
+                    aktifOgretmen = kullaniciAdi; // Aktif öğretmeni kaydet
+                    UygulamayiBaslat();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Kullanıcı adı veya şifre hatalı. Lütfen tekrar deneyin.");
+                }
+
+                if (i == 2)
+                {
+                    Console.WriteLine("3 kez yanlış giriş yaptınız. Program kapanıyor...");
+                    return;
+                }
+            }
+        }
+
+        static void UygulamayiBaslat()
+        {
+
+                Console.Write("Öğrenci No: ");
+                int oNo = int.Parse(Console.ReadLine());
+
+                Console.Write("Öğrenci Adı: ");
+                string oName = Console.ReadLine();
+
+                Console.Write("Öğrenci Soyadı: ");
+                string oSurname = Console.ReadLine();
+
+                Console.Write("Vize 1: ");
+                int vize1 = int.Parse(Console.ReadLine());
+
+                Console.Write("Vize 2: ");
+                int vize2 = int.Parse(Console.ReadLine());
+
+                Console.Write("Final: ");
+                int final = int.Parse(Console.ReadLine());
+
+                Console.Write("Okul Adı: ");
+                string okulName = Console.ReadLine();
+
+                ogrenci ogrenci1 = new ogrenci(oNo, oName, oSurname, vize1, vize2, final, okulName);
+
+
+            while (true)
+            {
+                if (!ogrenciBilgileri.ContainsKey(aktifOgretmen))
+                {
+                    ogrenciBilgileri[aktifOgretmen] = new Dictionary<int, ogrenci>();
+                }
+
+                ogrenciBilgileri[aktifOgretmen][oNo] = ogrenci1;
+
+                Console.WriteLine("Yapmak istediğiniz işlemi seçiniz");
+                islemler();
+                int secim = int.Parse(Console.ReadLine());
+
+                switch (secim)
+                {
+                    case 1:
+                        Console.WriteLine("Öğrenci bilgileri gösteriliyor");
+                        ogrenci1.ogrenciBilgileriGoster();
+                        break;
+                    case 2:
+                        double ogrenciOrt = ogrenci1.ogrenciOrtBul();
+                        Console.WriteLine("Ortalaması: " + ogrenciOrt);
+                        break;
+                    case 3:
+                        Console.WriteLine("Okulunun adı: " + okulName);
+                        break;
+                    case 4:
+                        Console.WriteLine("Ana ekrana dönülüyor...");
+                        Main();
+                        break;
+                    case 5:
+                       Console.WriteLine("Çıkış yapılıyor...");
+                        return;
+                    
+                    default:
+                        Console.WriteLine("Lütfen geçerli bir sayı giriniz!");
+                        break;
+                }
+            }
+        }
+
+        static void islemler()
+        {
+            Console.WriteLine("1: Öğrenci bilgisi göster");
+            Console.WriteLine("2: Öğrenci ortalamasını göster");
+            Console.WriteLine("3: Öğrenci okulunu göster");
+            Console.WriteLine("4: Öğretmen giriş sayfası");
+            Console.WriteLine("5: Çıkış yap");
+        }
+
+
+
+        static void KullaniciBilgileriEkle(string kullaniciAdi, string sifre)
+        {
+            kullaniciBilgileri[kullaniciAdi] = sifre;
+        }
+
+        static bool KullaniciGirisDogrulama(string kullaniciAdi, string sifre)
+        {
+            if (kullaniciBilgileri.ContainsKey(kullaniciAdi))
+            {
+                return kullaniciBilgileri[kullaniciAdi] == sifre;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ ogrenci.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgrenciSistemi
+{
+    public class ogrenci
+    {
+        private int oNo, vize1, vize2, final;
+        private string oName, okulName;
+        private string oSurname;
+
+        public ogrenci(int oNo, string oName, string oSurname, int vize1, int vize2, int final, string okulName)
+        {
+            this.oNo = oNo;
+            this.oName = oName;
+            this.oSurname = oSurname;
+            this.vize1 = vize1;
+            this.vize2 = vize2;
+            this.final = final;
+            this.okulName = okulName;
+
+        }
+
+        public void ogrenciBilgileriGoster()
+        {
+            Console.WriteLine($"Ogrenci adi {oName} ");
+            Console.WriteLine($"Ogrenci soyadi {oSurname} ");
+            Console.WriteLine($"Ogrenci no {oNo} ");
+            Console.WriteLine($"Ogrenci vize1 {vize1} ");
+            Console.WriteLine($"Ogrenci vize2 {vize2} ");
+            Console.WriteLine($"Ogrenci final {final} ");
+        }
+
+        public double ogrenciOrtBul()
+        {
+            double ort = (vize1 * 0.2 + vize2 * 0.2 + final * 0.6);
+            return ort;
+        }
+        public void okulgetir()
+        {
+            Console.WriteLine("Ogrencinin okulu " + okulName);
+        }
+
+
+    }
+}
